a5f16c46f354ddb8c8af87bc40a6394d02d688d9
RainWashed <kingretracted4yt@gmail.com>
2022-10-15 00:39:47 -0400

    i am very tired it is 12:40am and stuff, but started work on navigation method (will be a FAB, might dynamically change FAB or have a list of other smaller FABs when the main FAB is pressed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from "next/app";
 
-import FooterComponent from "./components/footer.component";
-
 import "../public/styles/root.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Component {...pageProps}></Component>
-            <FooterComponent />
+            <Component {...pageProps} />
         </>
     );
 }
---
+++ pages/components/fab.component.tsx
@@ -0,0 +1,3 @@
+function Fab(props) {
+    
+}
