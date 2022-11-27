fe354d7c025562696c5691fbdbc8f28c3b69224a
Rain <dementia.aswad@gmail.com>
2022-11-26 21:13:43 -0500

    completed enrollment api

--- src/helpers/api.route.ts
+++ src/helpers/api.route.ts
@@ -3,6 +3,7 @@ import { apiRouteMiddleware } from "./constants";
 import requestDemographicData from "./demographic.api";
 import requestContactsData from "./contacts.api";
 import requestScheduleData from "./schedule.api";
+import requestEnrollmentData from "./enrollment.api";
 
 function apiRoute(
     fastify: FastifyInstance,
@@ -86,6 +87,28 @@ function apiRoute(
                     error: true,
                     message: String(error),
                     reason: "/schedule GET failed.",
+                })
+            );
+        }
+    });
+
+    fastify.get("/enrollment", async (req, res) => {
+        try {
+            let call = await requestEnrollmentData(
+                req.headers["sessioncookie"] as string
+            );
+            res.code(200).send(
+                JSON.stringify({
+                    error: true,
+                    data: call,
+                })
+            );
+        } catch (error) {
+            res.code(500).send(
+                JSON.stringify({
+                    error: true,
+                    message: String(error),
+                    reason: "/enrollment GET failed.",
                 })
             );
         }
--- src/helpers/enrollment.api.ts
+++ src/helpers/enrollment.api.ts
@@ -19,6 +19,106 @@ async function requestEnrollmentData(sessionToken: string) {
             });
 
             let $ = cheerio.load(String(req.data));
+
+            let mainInformationTable = $(
+                "#Table > tbody > tr > td > table:nth-child(1) > tbody"
+            );
+            let lockerInformationTable = $(
+                "#Table > tbody > tr > td > table:nth-child(6)"
+            );
+            let enrollmentHistoryTable = $(
+                "#Table > tbody > tr > td > table:nth-child(11)"
+            );
+
+            let data: { [key: string]: { [key: string]: string | null } } = {
+                mainInformation: {},
+                lockerInformation: {},
+                enrollmentInformation: {},
+            };
+
+            mainInformationTable.children().each((_: number, row) => {
+                let labelElems = $(row)
+                    .children()
+                    .filter((__: number, elem) => {
+                        return (
+                            $(elem).attr("style")?.toLowerCase() ===
+                            "white-space:nowrap"
+                        );
+                    });
+
+                $(labelElems).each((_: number, elem) => {
+                    let label = $(elem).text();
+                    let value: string | null = $(elem).next().text();
+
+                    label = label
+                        .toLowerCase()
+                        .replace(labelFilteringCriteria, "")
+                        .trim();
+                    value = value.replace(valueFilteringCriteria, "").trim();
+                    if (value === "" || value === "<Unset>") {
+                        value = null;
+                    }
+
+                    data.mainInformation[label] = value;
+                });
+            });
+
+            let lockerInformationTableLabels = $(lockerInformationTable).find(
+                "thead > tr"
+            );
+            let lockerInformationTableValues = $(lockerInformationTable).find(
+                "tbody > tr"
+            );
+            let lockerInformationLabels: string[] = [];
+
+            lockerInformationTableLabels.children().each((_: number, label) => {
+                let _label = $(label)
+                    .text()
+                    .toLowerCase()
+                    .replace(labelFilteringCriteria, "")
+                    .trim();
+                lockerInformationLabels.push(_label);
+            });
+
+            lockerInformationTableValues.children().each((_: number, value) => {
+                let _value: string | null = $(value)
+                    .text()
+                    .replace(valueFilteringCriteria, "")
+                    .trim();
+
+                if (_value === "") {
+                    _value = null;
+                }
+
+                data.lockerInformation[lockerInformationLabels[_]] = _value;
+            });
+
+            let enrollmentHistoryTableLabels = $(enrollmentHistoryTable).find(
+                "thead > tr"
+            );
+            let enrollmentHistoryTableValues = $(enrollmentHistoryTable)
+                .find("tbody > tr")
+                .children();
+
+            enrollmentHistoryTableLabels.children().each((_: number, label) => {
+                let _label = $(label)
+                    .text()
+                    .toLowerCase()
+                    .replace(labelFilteringCriteria, "")
+                    .trim();
+                let _value: string | null = $(enrollmentHistoryTableValues[_])
+                    .text()
+                    .replace(valueFilteringCriteria, "")
+                    .trim();
+
+                if (_value === "") {
+                    _value = null;
+                }
+
+                data.enrollmentInformation[_label] = _value;
+            });
+
+            resolve(data);
         } catch (error: any) {
             if (error.response) {
                 reject(
@@ -34,3 +134,5 @@ async function requestEnrollmentData(sessionToken: string) {
         }
     });
 }
+
+export default requestEnrollmentData;
